cc0261f5d7ca8cacdad235ecb6d28075317f6292
fabiodelli <ntqdel@hotmail.com>
2023-03-22 00:42:53 +0100

    add disappear

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,14 @@
 //Definisco variabili utili
 
 const containerEl = document.querySelector(".container")
-const buttonEl = document.querySelector(".btn")
-const randomNumberAr = []
+const playEl = document.querySelector(".btn")
+const controlEl = document.getElementById("control")
+
 
 //Dichiaro funzione per generare numeri random diversi tra loro e metterli a schermo
 
 function generate (){
-
+const randomNumberAr = []
 while (randomNumberAr.length < 5) {
     const randomNumber = Math.floor(Math.random()*100+1)
     const ceck= randomNumberAr.includes(randomNumber)
@@ -17,12 +18,58 @@ while (randomNumberAr.length < 5) {
        const cell = `<div class="cell">${randomNumber}</div>`
        containerEl.innerHTML += (cell)
     }
-}}
+  }
+return randomNumberAr}
+
+
+//Creo funzione per far sparire i numeri allo scadere del tempo
+
+function disapper(){
+   setTimeout (countDown,5000)
+   function countDown (){
+   containerEl.innerHTML= ("")
+ }
+}
+
+//Chiedo all' utente di inserire i numeri precedentemente visualizzati e li pusho in un array
+function startGetNumber(){
+   let numberArray =[] 
+   setTimeout (getNumber,5100)
+   function getNumber(){
+      for (let i = 1; i <= 5; i++) {
+      const number = Number(prompt(`inserisci il ${i}° numero`))
+      numberArray.push(number) 
+   }
+   console.log(numberArray)
+ }
+return numberArray }
+
+//Scorro i numeri inseriti e controllo se sono all' interno del array dei numeri random
+function control(){
+   let correct=[]
+for (let i = 0; i < startGetNumber.length; i++) {
+   const currentNumber = startGetNumber[i];
+   const includes = generate.includes(currentNumber)
+   if(includes === true ){
+   correct.push(currentNumber)
+   console.log(correct)
+   }
+  }
+  if (correct.length == 5){
+   console.log("hai vinto")
+  } else {
+   console.log("hai perso")
+  }
+}
+
 
 //Richiamo funzione tramite click di un bottone
 
- buttonEl.addEventListener("click",generate) 
+playEl.addEventListener("click",generate) 
 
- //Creo funzione per far sparire i numeri allo scadere del tempo
+playEl.addEventListener("click",disapper) 
 
- setTimeout 
+playEl.addEventListener("click",startGetNumber) 
+
+controlEl.addEventListener("click",control)
+
